2230297fc4bd1a55154f87ef1fa9c6d974e27694
Justin Liu <justinliu@Justins-MacBook-Air-7.local>
2026-03-25 14:19:50 +1100

    feat: add password and JWT utility functions

---
+++ backend/app/services/auth/utils.py
@@ -0,0 +1,25 @@
+from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "CodeRoyale"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(plain_password: str):
+    return pwd_context.hash(plain_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode["exp"] = expire
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
