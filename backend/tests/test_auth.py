615001a2b141cf369f0c803719dfb3424cebcde9
Justin Liu <justin.liu218@gmail.com>
2026-04-07 19:45:26 +1000

    refactor: return (response, json) from test utils and update auth tests

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -2,62 +2,60 @@ from tests.utils import signup_user, login_user, assert_validation_error
 
 # Test new user signup
 def test_signup_success(client):
-    signup_response = signup_user(client)
+    signup_response, signup_json = signup_user(client)
     assert signup_response.status_code == 200
-    signup_json = signup_response.get_json()
     assert signup_json["username"] == "TestUser"
     assert "hashed_password" not in signup_json # Ensure password isn't exposed
 
 # Test existing user signup
 def test_signup_duplicate(client):
     signup_user(client)
-    signup_response = signup_user(client)
+    signup_response, _ = signup_user(client)
     assert signup_response.status_code == 400
     assert "already registered" in signup_response.text
 
 # Test signup with username too short
 def test_signup_username_too_short(client):
-    signup_response = signup_user(client, username="a")
+    signup_response, _ = signup_user(client, username="a")
     assert_validation_error(signup_response)
     assert "at least 2 characters" in signup_response.text
 
 # Test signup with username too long
 def test_signup_username_too_long(client):
-    signup_response = signup_user(client, username="a"*129)
+    signup_response, _ = signup_user(client, username="a"*129)
     assert_validation_error(signup_response)
     assert "at most 128 characters" in signup_response.text
 
 # Test signup with password too short
 def test_signup_password_too_short(client):
-    signup_response = signup_user(client, password="a")
+    signup_response, _ = signup_user(client, password="a")
     assert_validation_error(signup_response)
     assert "at least 8 characters" in signup_response.text
 
 # Test signup with password too long
 def test_signup_password_too_long(client):
-    signup_response = signup_user(client, password="a"*257)
+    signup_response, _ = signup_user(client, password="a"*257)
     assert_validation_error(signup_response)
     assert "at most 256 characters" in signup_response.text
 
 # Test correct credentials login
 def test_login_success(client):
     signup_user(client)
-    login_response = login_user(client)
+    login_response, login_json = login_user(client)
     assert login_response.status_code == 200
-    login_json = login_response.get_json()
     assert "access_token" in login_json # Ensure token is returned
     assert login_json["token_type"] == "bearer"
 
 # Test wrong username login
 def test_login_with_wrong_username(client):
     signup_user(client)
-    login_response = login_user(client, username="Lemon")
+    login_response, _ = login_user(client, username="Lemon")
     assert login_response.status_code == 401
     assert "Incorrect username or password" in login_response.text
 
 # Test wrong password login
 def test_login_with_wrong_password(client):
     signup_user(client)
-    login_response = login_user(client, password="Lime12345")
+    login_response, _ = login_user(client, password="Lime12345")
     assert login_response.status_code == 401
     assert "Incorrect username or password" in login_response.text
--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -3,16 +3,23 @@ def signup_user(client, username="TestUser", password="Password123"):
         "username": username,
         "password": password
     }
-    return client.post("/auth/signup", json=signup_data)
+    response = client.post("/auth/signup", json=signup_data)
+    return response, response.get_json()
 
 def login_user(client, username="TestUser", password="Password123"):
     login_data = {
         "username": username,
         "password": password
     }
-    return client.post("/auth/token", json=login_data)
+    response = client.post("/auth/token", json=login_data)
+    return response, response.get_json()
 
 def assert_validation_error(response):
     assert response.status_code == 400
     json_data = response.get_json()
     assert "validation_error" in json_data
+
+def queue_user(client, token):
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.post("/matchmaking/join_queue", headers=headers)
+    return response, response.get_json()
